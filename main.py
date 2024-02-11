8854666eca20948cd8da419bcf18f3c7e5a9dea7
TavinV <otavioviniciusads@gmail.com>
2024-02-10 21:44:38 -0300

    modularization

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-square = float(input("Chose a number for calculating the square root: "))
+def square_root(square: float, n_iterations: int) -> float:
+    """Evaluates the square root of the given input, for n iterations,
+    with the babylonian method\n
+    Parameters:
+        square (float): the square
+        n_iterations (int): the number of iterations for the algorithm
+    Returns:
+        float: the square root of the input"""
+    
+    # initial guess
+    square_root = 1
+    
+    # iterates the algorithm
+    for i in range(n_iterations):
+        print(square_root)
+        square_root = 0.5*(square_root + (square/square_root))
 
-# initial guess
-square_root = 1
+    print(f'sqrt of {square} is {square_root}')
 
-n_iterations = int(input("How many iterations? "))
+    return square_root
 
-for i in range(n_iterations):
-    print(square_root)
-    square_root = 0.5*(square_root + (square/square_root))
+def main():
+    square = float(input("Chose a number for calculating the square root: "))
+    n_iterations = int(input("How many iterations? "))
+    square_root(square=square, n_iterations=n_iterations)
 
-print(f'sqrt of {square} is {square_root}')
+main()
